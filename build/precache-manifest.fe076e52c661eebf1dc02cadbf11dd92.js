e6a951a51ee736b0138dfe125ad4b4f9045fc885
james.s.debettencourt <james.s.debettencourt@gmail.com>
2020-01-27 20:45:15 -0500

    added react build

---
+++ build/precache-manifest.fe076e52c661eebf1dc02cadbf11dd92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2b14c9378a109d45a232700bc468a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "762e052ccc323405e239",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50a7efb4afd3e40d9ed8",
+    "url": "/static/js/2.f09326fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f09326fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "762e052ccc323405e239",
+    "url": "/static/js/main.4dec2a01.chunk.js"
+  },
+  {
+    "revision": "5b042db3b389004e2277",
+    "url": "/static/js/runtime-main.d41e82c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
